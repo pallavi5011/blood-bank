7c4c74ba0703b17b6cbb1e2c84e4a9e05cbe4019
pallavi5011 <pallawi758@gmail.com>
2024-04-03 02:24:14 +0530

    reuseable comp&forms

---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Form from '../../components/shared/Form/Form'
+
+
+const Login = () => {
+    return (
+        <>
+            <div className='row'>
+                <div className='col-md-8 form-banner'>
+                    <img src="./assets/images//banner1.jpg" alt='loginImage' />
+                </div>
+                <div className="col-md-4 form-container">
+                <Form
+                formTitle={'Login Page'} submitBtn = {"Login"}
+                />
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Login
