5b0e4e7009a01d9e045689305b96ab8d0258e207
pallavi5011 <pallawi758@gmail.com>
2024-04-23 02:27:40 +0530

    bug fixing

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Spinner from '../components/shared/Spinner'
 
 const HomePage = () => {
+  const { loading, error } = useSelector((state) => state.auth)
   return (
     <>
-    <h1>Home page</h1>
+      {error && <span> {alert(error)} </span>}
+      {/* {error && toast.error(error)} */}
+      {loading ? (<Spinner />) : (
+        <h1>Home page</h1>
+      )}
     </>
   )
 }
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Form from '../../components/shared/Form/Form'
+import { useSelector } from 'react-redux'
+import Spinner from '../../components/shared/Spinner'
 
 const Register = () => {
+  const { loading, error } = useSelector((state) => state.auth)
   return (
     <div>
       <>
+      {error && <span> {alert(error)} </span>}
+        {/* {error && toast.error(error)} */}
+            {loading ? (<Spinner />) : (
         <div className='row g-0'>
           <div className='col-md-8 form-banner'>
             <img src="./assets/images/banner1.jpg" alt="loginImage" />
@@ -14,6 +20,7 @@ const Register = () => {
             <Form formTitle={'Register'} submitBtn={'Register'} formType={"register"}/>
           </div>
         </div>
+            )}
       </>
     </div>
   )
