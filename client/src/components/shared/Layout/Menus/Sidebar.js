fde77436d5bf894a99ff05770479781ff196fa33
pallavi5011 <pallawi758@gmail.com>
2024-04-26 01:46:56 +0530

    blood record modal & blood in out record

--- client/src/components/shared/Layout/Menus/Sidebar.js
+++ client/src/components/shared/Layout/Menus/Sidebar.js
@@ -4,27 +4,27 @@ import { useLocation,Link } from 'react-router-dom'
 import '../../../../styles/Layout.css'
 
 const Sidebar = () => {
-    const location = useLocation();
-    return (
-      <div>
-        <div className="sidebar">
-          <div className="menu">
-            {userMenu.map((menu) => {
-              const isActive = location.pathname === menu.path;
-              return (
-                <div
-                  className={`menu-item ${isActive && "active"}`}
-                  key={menu.name}
-                >
-                  <i className={menu.icon}></i>
-                  <Link to={menu.path}>{menu.name}</Link>
-                </div>
-              );
-            })}
-          </div>
+  const location = useLocation();
+  return (
+    <div>
+      <div className="sidebar">
+        <div className="menu">
+          {userMenu.map((menu) => {
+            const isActive = location.pathname === menu.path;
+            return (
+              <div
+                className={`menu-item ${isActive && "active"}`}
+                key={menu.name}
+              >
+                <i className={menu.icon}></i>
+                <Link to={menu.path}>{menu.name}</Link>
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-  };
-  
-  export default Sidebar;
+    </div>
+  );
+};
+
+export default Sidebar;
