2dbc1f37e9448ed56c4b92ab55c9c19c7ab79273
pallavi5011 <pallawi758@gmail.com>
2024-05-07 17:17:25 +0530

    add pre installation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require('colors')
 const morgan = require('morgan')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const path = require('path')
 
 dotenv.config();
 
@@ -24,6 +25,14 @@ app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
+
+  //STATIC FOLDER
+  app.use(express.static(path.join(__dirname, './client/build')))
+
+  //STATIC ROUTES
+  app.get('*', function(req,res){
+   res.sendFile(path.join(__dirname, './client/build/index.html'))
+  })
 
  const PORT = process.env.PORT || 8080;
 
